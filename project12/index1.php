80f80ea8074bc3bf8c7404aab6f4b2434c78e40c
unknown <nghanh762003.it@gmail.com>
2024-02-23 09:11:03 +0700

    them file index1.php

---
+++ project12/index1.php
@@ -0,0 +1,19 @@
+<?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+echo '<nav><ul>';
+foreach ($navItems as $item) {
+    echo "<li>$item</li>";
+}
+echo '</ul></nav>';
+echo "hello teams"
+    ?>
