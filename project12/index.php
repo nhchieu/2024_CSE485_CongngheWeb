945b446b3156645fb54de52255a6d7a099426f56
nhchieu <nhchieu271@gmail.com>
2024-02-23 20:43:24 +0700

    update header project12

--- project12/index.php
+++ project12/index.php
@@ -1,3 +1,7 @@
+
+<?php
+    include("header.php");
+?>
 <?php
 $navItem = [
     "GIỚI THIỆU",
@@ -12,18 +16,7 @@ $navItem = [
 ];
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="https://fontawesome.com/icons" crossorigin="anonymous"></script>
-</head>
-
-<body>
     <div class="menubar">
         <nav>
             <ul>
