11972fd0c30d6d913c978016ee1c7a51a3a4b8aa
Hai Anh <nghanh762003.it@gmail.com>
2024-02-23 09:24:05 +0700

    them index1 vao project13

---
+++ project12/index1.php
@@ -0,0 +1,26 @@
+<?php
+$navItem = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐẠO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+
+?>
+<nav>
+    <ul>
+        <?php
+        foreach ($navItem as $item) {
+            echo '<li>$item</li>';
+        }
+        ?>
+    </ul>
+</nav>
+
+<?php
+echo "Hello World!";
