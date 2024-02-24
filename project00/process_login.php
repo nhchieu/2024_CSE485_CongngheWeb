7b485254120a1cb03ea675409b5d549b914222b8
Hai Anh <nghanh762003.it@gmail.com>
2024-02-24 09:37:12 +0700

    chinhsua process login, them 1 so comment

--- project00/process_login.php
+++ project00/process_login.php
@@ -1,10 +1,12 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //lay ra du lieu tu form
     $user = $_POST['user'];
     $pass = $_POST['pass'];
-    if ($user == 'haianh' && $pass == '123') {
+    //kiem tra csdl (chua hoc)
+    if ($user == 'haianh' && $pass == '123') { //thu cccd
         session_start();
-        $_SESSION['isLogin'] = $user;
-        header('Location:admin/index1.php');
+        $_SESSION['isLogin'] = $user; //cap the
+        header('Location:project00/index1.php'); //chuyen huong mac dinh
     }
 }
