32a4d7662c6af200c6d4b2638d15bbf476eb4063
Hai Anh <nghanh762003.it@gmail.com>
2024-02-24 09:44:34 +0700

    chinh sua chuc nang login

--- project00/login.php
+++ project00/login.php
@@ -1,3 +1,5 @@
 <form action="process_login.php" method="post">
     Username: <input type="text" name="user" id="">
-    Password: <input type="password"
+    Password: <input type="text" name="pass">
+    <button type="submit">Login</button>
+</form>
