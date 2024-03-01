fe4d236293fc7b346eee488613717d6bcdfd6f1b
Hai Anh <nghanh762003.it@gmail.com>
2024-03-01 07:59:44 +0700

    modified change in form

--- project22/form.php
+++ project22/form.php
@@ -1,3 +1,12 @@
+<?php
+$user = [
+    "id" => 1,
+    "name" => "John Doe",
+    "email" => "johndoe@example.com",
+    "avatar" => "uploads/default_avatar.png" // Initial avatar URL
+   ];
+?>
+
 <form action="update_profile.php" method="post" enctype="multipart/form-data">
     <h2>Profile Information</h2>
     <label for="name">Name:</label>
